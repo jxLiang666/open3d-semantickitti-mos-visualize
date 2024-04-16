8935ef9229c141d892378504bd58ed2e555e1552
Liangjinxin <574347455@qq.com>
2024-04-16 17:48:54 +0800

    upgrade

--- viz.sh
+++ viz.sh
@@ -1,7 +1,6 @@
 seq=08
-datasetname=kittidata
-dataset_path=/home/liang/$datasetname/sequences/$seq/velodyne
-gt_label_path=/home/liang/$datasetname/sequences/$seq/labels
-prediction_label_path=/home/liang/$datasetname/predictions/sequences/$seq
+dataset_path=/home/liang/kittidata/sequences/$seq/velodyne
+gt_label_path=/home/liang/kittidata/sequences/$seq/labels
+prediction_label_path=/home/liang/kittidata/predictions/sequences/$seq
 
 python viz.py -d $dataset_path -gt $gt_label_path -pr $prediction_label_path
